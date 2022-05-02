ec1ed332681812e9769046c94275b882e80fd3b5
Jadon Yack <jadon.yack@gmail.com>
2022-05-02 16:50:15 -0400

    Add afk command dictionary management. Add afk command confirmation
    Doesn't do anything with known afk users yet, needs implementation.
    Reacts with a thumbs up to confirm that the afk command did in fact work.
    Utilizes a dictionary to store afk users.

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
-from http import client
+#!/bin/python3
 import discord
-
 import os
 
+from http import client
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -10,24 +10,47 @@ load_dotenv('./botstuff.env')
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix='//')
+bot = commands.Bot(command_prefix='$')
 
-@bot.event
-async def on_ready():
-    print('Logged on as bot.py!')
-
-
-@bot.event
-async def on_message(message):
-    print('Message from {0.author}: {0.content}'.format(message))
-
-    if 'hello there' in message.content.lower():
-        await message.channel.send(file=
-                discord.File(fp=open('GIFs/general-kenobi.gif', 'rb')))
-            
-afkPeeps = {}
-@bot.command(name = 'AFK')
+# Usage: //afk <reason> 
+# Inserts the user into the afk_peeps dictionary, along with their reason 
+# for being afk. If this user is mentioned, relay a message stating that user's
+# reason.
+afk_peeps = {}
+@bot.command(name = 'afk')
 async def afk(ctx):
-    global afkPeeps
+    global afk_peeps
+    if ctx.author not in afk_peeps.keys():
+        afk_peeps[ctx.author] = ctx.message.content
+    else:
+        afk_peeps.pop(ctx.author)
     
+    # Allows the user to know that the bot actually knows they're afk
+    await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
+
+   
+@bot.command(name='ping')
+async def ping(ctx):
+    print('Entered function ping()!')
+    await ctx.send('pong')
+
+# Confirms that the bot is logged on and ready to be used                       
+@bot.event                                                                      
+async def on_ready():                                                           
+    print('Logged on as bot.py!')                                               
+                                                                                
+# Checks incoming messages to passively respond based on the message. More to   
+# be added.                                                                     
+@bot.event                                                                      
+async def on_message(message):                                                  
+    print('Message from {0.author}: {0.content}'.format(message))               
+                                                                                 
+    if 'hello there' in message.content.lower():                                
+        await message.channel.send(file=                                        
+                discord.File(fp=open('GIFs/general-kenobi.gif', 'rb')))
+
+    # Required in order for commands to be processed due to overriding 
+    # on_message.
+    await bot.process_commands(message)
+
 bot.run(DISCORD_TOKEN)
