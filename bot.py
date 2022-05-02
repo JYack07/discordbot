0fb9b02022d76b1d87893956fd82071197d94571
Jadon Yack <jyack@umich.edu>
2022-05-01 22:07:44 -0400

    Add "General Kenobi" GIF response

--- bot.py
+++ bot.py
@@ -1,11 +1,17 @@
 import discord
 
-class MyClient(discord.client):
+class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
+        if message.author == self.user:
+            return
+
+        if message.content.lower().startswith('hello there'):
+            await message.channel.send(file=
+                    discord.File(fp=open('GIFs/general-kenobi.gif', 'rb')))
 
 client = MyClient()
 client.run('OTcwNDc5NjI0MzMxMDk2MTE1.Ym8jlQ.bkDThAXsGQbedL6KxfacozL5Zac')
